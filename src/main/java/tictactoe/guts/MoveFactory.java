bf0c91b23c48fe118f61ac311f709d74e012a6bd
Max Trapido <maxtrapido@gmail.com>
2021-09-15 16:58:52 -0700

    Create Showable, Show, and ShowBreakChain classes

--- src/main/java/tictactoe/guts/MoveFactory.java
+++ src/main/java/tictactoe/guts/MoveFactory.java
@@ -2,9 +2,6 @@ package tictactoe.guts;
 
 import java.util.ArrayDeque;
 import java.util.Queue;
-import java.util.function.BiConsumer;
-import java.util.function.Consumer;
-import java.util.function.Supplier;
 
 public class MoveFactory {
 
@@ -21,56 +18,20 @@ public class MoveFactory {
         moveAndShows = new ArrayDeque<>();
     }
 
-
-    //thenShowThenAsync() documentation draft:
-    //Same as thenShow, except that it will  allow you to call the next method asynchronously
-    //The show part is still called synchronously in relation to the previous move
-    //*** THIS METHOD MUST BE CALLED WHEN THE NEXT METHOD IS ASYNC, EVEN IF THIS ONE ISN'T ***
-    //This method acts as a turning point for this class. Before it, all methods well be synced with the initial move() call
-    //All methods after will be synced with relation to when this runnable is called
-
-    public class Show<T> {
-        private final Supplier<? extends T> toShow;
-        private Consumer<? super T> thenShow;
-        private BiConsumer<? super T, Runnable> thenShowThenAsync;
-
-        private Show(Supplier<? extends T> toShow) {
-            this.toShow = toShow;
-        }
-
-        public MoveFactory thenShow(Consumer<? super T> thenShow) {
-            this.thenShow = thenShow;
-            return MoveFactory.this;
-        }
-
-        public MoveFactory thenShowThenAsync(BiConsumer<? super T, Runnable> thenShowThenAsync) {
-            this.thenShowThenAsync = thenShowThenAsync;
-            return MoveFactory.this;
-        }
-
-        private void moveAndShow() {
-            thenShow.accept(toShow.get());
-        }
-
-        private void moveAndShowAsync() {
-            thenShowThenAsync.accept(toShow.get(), MoveFactory.this::move);
-        }
-    }
-
     public Show<TicTacToeGame> startNewGame() {
-        return addMoveAndShowToQueue(new Show<>(() -> game = new TicTacToeGame()));
+        return addMoveAndShowToQueue(new Show<>(this, () -> game = new TicTacToeGame()));
     }
 
     public Show<Move> playerMove(int gridPositionId) {
-        return addMoveAndShowToQueue(new Show<>(() -> game.playerMove(gridPositionId)));
+        return addMoveAndShowToQueue(new Show<>(this, () -> game.playerMove(gridPositionId)));
     }
 
     public Show<Move> computerMove() {
-        return addMoveAndShowToQueue(new Show<>(game::computerMove));
+        return addMoveAndShowToQueue(new Show<>(this, game::computerMove));
     }
 
     public Show<GameEnd> checkGameEnd() {
-        return addMoveAndShowToQueue(new Show<>(game::getGameEnd));
+        return addMoveAndShowToQueue(new Show<>(this, game::getGameEnd));
     }
 
 
@@ -84,11 +45,10 @@ public class MoveFactory {
 
     public void move() {
         Show<?> nextShow;
-        while ((nextShow = moveAndShows.poll()) != null && nextShow.thenShow != null) {
-            nextShow.moveAndShow();
-        }
-        if (nextShow != null) {
-            nextShow.moveAndShowAsync();
+        while ((nextShow = moveAndShows.poll()) != null) {
+            nextShow.getToShow();
+            if (nextShow.isBreakChain())
+                return;
         }
     }
 
